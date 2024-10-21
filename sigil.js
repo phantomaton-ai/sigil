cca3933d6aa3306e563c07af21c9405cc4a56d60
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-20 19:15:33 -0700

    Updated by Phantomaton

--- sigil.js
+++ sigil.js
@@ -1,17 +1,39 @@
-const sigil = (name) => {
-  const symbol = Symbol(name);
-  return {
-    as: (target) => ({
-      like: (provider) => {
-        target[symbol] = provider;
-      }
-    }),
-    using: (instance) => ({
-      upon: (input) => {
-        return instance.constructor[symbol](input);
-      }
-    })
-  };
-};
+class As {
+  constructor(sigil, target) {
+    this.sigil = sigil;
+    this.target = target;
+  }
+
+  like(provider) {
+    this.target[this.sigil.symbol] = provider;
+  }
+}
+
+class Using {
+  constructor(sigil, instance) {
+    this.sigil = sigil;
+    this.instance = instance;
+  }
+
+  upon(input) {
+    return this.instance.constructor[this.sigil.symbol](input);
+  }
+}
+
+class Sigil {
+  constructor(name) {
+    this.symbol = Symbol(name);
+  }
+
+  as(target) {
+    return new As(this, target);
+  }
+
+  using(instance) {
+    return new Using(this, instance);
+  }
+}
+
+const sigil = (name) => new Sigil(name);
 
 export default sigil;
